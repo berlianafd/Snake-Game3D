63ec36cb4f5add1fd06dfb81d5205c1114c95fe3
berlianafd <berlianafd@gmail.com>
2020-03-23 12:34:12 +0700

    Initial commit

---
+++ Assets/Scripts/Score.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Score : MonoBehaviour {
+
+    private LevelManager levelManager;
+    public static int score = 0;
+
+	// Use this for initialization
+	void Start () {
+		GameObject.DontDestroyOnLoad(gameObject);
+
+        //get level manager
+        levelManager = GameObject.Find("LevelManager").GetComponent<LevelManager>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		//get the score
+		score = FruitRotator.count;
+        NextLevel();
+	}
+
+    void NextLevel()
+    {
+        //Debug.Log("Triggredd");
+        //collide with wall or rock
+        if (score == 10)
+        {
+            Debug.Log("Next Level");
+            //goto lose screen
+            levelManager.LoadLevel("game2");
+        }
+    }
+
+}
